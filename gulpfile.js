21bb2ba8cc54440adfdf5540d7df35ac9d8e1268
ernscht <ernscht@gmail.com>
2015-06-03 09:49:55 +0200

    nitro update

--- gulpfile.js
+++ gulpfile.js
@@ -26,12 +26,11 @@ var gulp = require('gulp'),
 
 
 function getSourceFiles(ext) {
-	var assetsConfig = require('./config.json').assets,
-		assets = [];
+	var assets = [];
 
-	for (var key in assetsConfig) {
-		if (assetsConfig.hasOwnProperty(key) && ext === path.extname(key)) {
-			var asset = assetsConfig[key],
+	for (var key in cfg.assets) {
+		if (cfg.assets.hasOwnProperty(key) && ext === path.extname(key)) {
+			var asset = cfg.assets[key],
 				result = {
 					name: key,
 					deps: [],
@@ -170,8 +169,18 @@ gulp.task('watch', ['assets'], function () {
 		}
 	};
 
+
 	gulp.watch([
 		'config.json',
+	]).on('change', function(e) {
+		if ('changed' === e.type) {
+			cfg = cfg.reload();
+			gulp.start('compile-css');
+			gulp.start('compile-js');
+		}
+	});
+
+	gulp.watch([
 		'assets/**/*.less',
 		'components/**/*.less'
 	], ['compile-css'])
@@ -180,7 +189,6 @@ gulp.task('watch', ['assets'], function () {
 		});
 
 	gulp.watch([
-		'config.json',
 		'assets/**/*.js',
 		'components/**/*.js'
 	], ['compile-js'])
