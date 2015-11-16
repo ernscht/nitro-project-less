d6a5c2ec9d7aec6bd34eda167fe136530b7a5d56
ernscht <ernscht@gmail.com>
2015-11-16 18:21:19 +0100

    add antother test component

---
+++ components/molecules/Nav.Ma/doc/README.md
@@ -0,0 +1,10 @@
+Component: Nav.Ma
+Type:      molecule
+File:      navma
+
+User:      ernscht
+Email:     ernscht@gmail.com
+
+Prefix:    m
+JS:        NavMa
+CSS:       nav-ma
---
+++ components/molecules/Nav.Ma/js/navma.js
@@ -0,0 +1,18 @@
+(function($) {
+	'use strict';
+	/**
+	 * Nav.Ma module implementation.
+	 *
+	 * @author ernscht <ernscht@gmail.com>
+	 * @namespace T.Module
+	 * @class NavMa
+	 * @extends T.Module
+	 */
+	T.Module.NavMa = T.createModule({
+		start: function(resolve) {
+			var $ctx = $(this._ctx);
+
+			resolve();
+		}
+	});
+}(jQuery));
