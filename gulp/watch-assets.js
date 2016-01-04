abe8a771d142a299d9c738bbc6012cbb4fd62f53
ernscht <ernscht@gmail.com>
2016-01-04 09:44:55 +0100

    update nitro #22

---
+++ gulp/watch-assets.js
@@ -0,0 +1,60 @@
+var browserSync = require('browser-sync');
+var cfg = require('../app/core/config');
+
+module.exports = function (gulp, plugins) {
+	return function () {
+		var clearCache = function (e) {
+			if ('delete' === e.type) {
+				delete plugins.cached.caches.scripts[e.path];
+				plugins.remember.forget('scripts', e.path);
+			}
+		};
+
+		plugins.watch([
+			'config.json'
+		], function () {
+			cfg = cfg.reload();
+			gulp.start('compile-css');
+			gulp.start('compile-js');
+		});
+
+		plugins.watch([
+			'assets/**/*.less',
+			'components/**/*.less'
+		], function (e) {
+			clearCache(e);
+			gulp.start('compile-css');
+		});
+
+		plugins.watch([
+			'assets/**/*.js',
+			'components/**/*.js',
+			'!components/**/spec/*.js'
+		], function () {
+			
+			gulp.start('compile-js');
+		});
+
+		plugins.watch([
+			'views/**/*.html',
+			'!' + cfg.nitro.view_partials_directory + '/*.html', // exclude partials
+			'views/_data/**/*.json',
+			'components/**/*.html',
+			'components/**/_data/*.json'
+		], function () {
+			browserSync.reload();
+		});
+
+		plugins.watch([
+			'assets/img/**/*'
+		], function () {
+			gulp.start('minify-img');
+		});
+
+		plugins.watch([
+			'assets/font/**/*'
+		], function () {
+			gulp.start('assets-copy');
+		});
+	};
+};
