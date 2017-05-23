8e6a155c5ebb5771a9b171a412958d752aa37064
ernscht <ernscht@gmail.com>
2017-05-23 22:10:47 +0200

    nitro update

--- gulp/minify-js.js
+++ gulp/minify-js.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const utils = require('./utils');
+const Promise = require('es6-promise').Promise;
 const uglifyOptions = {
 	output: {
 		comments: /^!/
@@ -10,18 +11,24 @@ const uglifyOptions = {
 module.exports = (gulp, plugins) => {
 	return () => {
 		const assets = utils.getSourcePatterns('js');
+		let promises = [];
 
 		assets.forEach((asset) => {
-			gulp
-				.src(`public/assets/js/${asset.name}`)
-				.pipe(plugins.uglify(uglifyOptions))
-				.pipe(plugins.rename(asset.name.replace('.js', '.min.js')))
-				.pipe(plugins.size({showFiles: true, gzip: false, title: 'JavaScript minified'}))
-				.pipe(plugins.size({showFiles: true, gzip: true, title: 'JavaScript minified'}))
-				.pipe(gulp.dest('public/assets/js/'));
+			promises.push(new Promise((resolve) => {
+				gulp
+					.src(`public/assets/js/${asset.name}`)
+					.pipe(plugins.uglify(uglifyOptions))
+					.pipe(plugins.rename(asset.name.replace('.js', '.min.js')))
+					.pipe(plugins.size({showFiles: true, gzip: false, title: 'JavaScript minified'}))
+					.pipe(plugins.size({showFiles: true, gzip: true, title: 'JavaScript minified'}))
+					.pipe(gulp.dest('public/assets/js/'))
+					.on('end', () => {
+						resolve();
+					});
+			}));
 		});
 
-		return gulp;
+		return Promise.all(promises);
 	};
 };
 
