6e58254575e6b3c717891a2ada8ff1df9042335b
ernscht <ernscht@gmail.com>
2017-10-01 16:10:57 +0200

    nitro update

--- src/proto/js/prototype.js
+++ src/proto/js/prototype.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
-console.log('You are using nitro version 2.1.1.');
+console.log('You are using nitro version 3.0.0-alpha');
 console.log('Look for updates on https://npmjs.com/package/generator-nitro');
 /* eslint-enable no-console */
