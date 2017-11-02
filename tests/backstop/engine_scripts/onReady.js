d5fcfdfda119344776a722d0f26b43b3afafc8d2
ernscht <ernscht@gmail.com>
2017-11-02 23:25:10 +0100

    #114 backstop

--- tests/backstop/engine_scripts/onReady.js
+++ tests/backstop/engine_scripts/onReady.js
@@ -1,7 +1,7 @@
 'use strict';
 
-module.exports = function (engine, scenario, vp) {
-	engine.evaluate(function () {
+module.exports = (engine, scenario, vp) => {
+	engine.evaluate(() => {
 		// Your web-app is now loaded. Edit here to simulate user interacions or other state changes.
 	});
 	console.log('onReady.js has run for: ', vp.label);
