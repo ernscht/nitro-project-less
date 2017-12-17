52c0553d1b79df4565891c10d8ae2df49288d38b
ernscht <ernscht@gmail.com>
2017-12-17 12:16:31 +0100

    update nitro to 3.0.3

--- src/proto/js/proto-cheat.js
+++ src/proto/js/proto-cheat.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 window.cheet('n i t r o', () => {
-	console.log('Your are using nitro version 2.1.1.');
+	console.log('Your are using nitro version 3.0.3');
 	console.log('Look for updates on https://npmjs.com/package/generator-nitro');
 });
 /* eslint-enable no-console */
