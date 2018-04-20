d3eadac7149927d2d041cd3eab417f4208b27649
ernscht <ernscht@gmail.com>
2018-04-20 08:20:37 +0200

    update to nitro 3.5.1

--- tests/karma/karma.conf.js
+++ tests/karma/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
 		// list of files / patterns to load in the browser
 		files: [
 			'public/assets/js/vendor.js',
-			'public/assets/js/app.js',
+			'public/assets/js/ui.js',
 			'src/patterns/**/test/*.test.js',
 		],
 
